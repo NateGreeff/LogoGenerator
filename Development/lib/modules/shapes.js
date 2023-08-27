980e0cb127490a013fee05cd7b799003e952837a
Nathan Greeff <nathangreeff@hotmail.com>
2023-08-27 16:23:10 -0600

    Fixed shape generation

--- Development/lib/modules/shapes.js
+++ Development/lib/modules/shapes.js
@@ -1,22 +1,19 @@
 function generateShape(shape, shapeColor, logoText, textColor) {
-    let svg = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n`;
-
+    let svg = `<svg version="1.1" width="400" height="300" xmlns="http://www.w3.org/2000/svg">\n`;
     switch (shape) {
         case "Circle":
             svg += `    <circle cx="150" cy="100" r="80" fill="${shapeColor}" />\n`;
+            svg += `    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${logoText}</text>\n</svg>`;
             break;
         case "Triangle":
             svg += `    <polygon points="150,20 250,180 50,180" fill="${shapeColor}" />\n`;
+            svg += `    <text x="150" y="160" font-size="60" text-anchor="middle" fill="${textColor}">${logoText}</text>\n</svg>`;
             break;
         case "Square":
-            svg += `    <rect x="50" y="50" width="200" height="100" fill="${shapeColor}" />\n`;
+            svg += `    <rect x="50" y="50" width="200" height="200" fill="${shapeColor}" />\n`;
+            svg += `    <text x="150" y="170" font-size="60" text-anchor="middle" fill="${textColor}">${logoText}</text>\n</svg>`;
             break;
-        default:
-            throw new Error("Invalid shape");
     }
-
-    svg += `    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${logoText}</text>\n</svg>`;
     return svg
 }
-
 module.exports = generateShape
