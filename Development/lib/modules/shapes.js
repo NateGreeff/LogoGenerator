4a37d228f3ceeccc9909afb246d4f5d342caee0f
Nathan Greeff <nathangreeff@hotmail.com>
2023-08-27 19:18:26 -0600

    added comments to shapes

--- Development/lib/modules/shapes.js
+++ Development/lib/modules/shapes.js
@@ -1,3 +1,4 @@
+// Define a Shape class that will be inherited by other shape classes
 class Shape {
     constructor(shapeColor, logoText, textColor) {
         this.shapeColor = shapeColor;
@@ -5,16 +6,19 @@ class Shape {
         this.textColor = textColor;
     }
 
+    // Generate the basic SVG code for all shapes
     generateSVG() {
         return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n`;
     }
 }
 
+// Define a Circle class that inherits from Shape
 class Circle extends Shape {
     constructor(shapeColor, logoText, textColor) {
         super(shapeColor, logoText, textColor);
     }
 
+    // Generate the SVG code for a circle
     generateSVG() {
         let svg = super.generateSVG();
         svg += `    <circle cx="150" cy="100" r="80" fill="${this.shapeColor}" />\n`;
@@ -23,11 +27,13 @@ class Circle extends Shape {
     }
 }
 
+// Define a Triangle class that inherits from Shape
 class Triangle extends Shape {
     constructor(shapeColor, logoText, textColor) {
         super(shapeColor, logoText, textColor);
     }
 
+    // Generate the SVG code for a triangle
     generateSVG() {
         let svg = super.generateSVG();
         svg += `    <polygon points="150,20 250,180 50,180" fill="${this.shapeColor}" />\n`;
@@ -36,11 +42,13 @@ class Triangle extends Shape {
     }
 }
 
+// Define a Square class that inherits from Shape
 class Square extends Shape {
     constructor(shapeColor, logoText, textColor) {
         super(shapeColor, logoText, textColor);
     }
 
+    // Generate the SVG code for a square
     generateSVG() {
         let svg = super.generateSVG();
         svg += `    <rect x="50" y="50" width="150" height="150" fill="${this.shapeColor}" />\n`;
@@ -49,6 +57,7 @@ class Square extends Shape {
     }
 }
 
+// Export the Circle, Triangle, and Square classes for use in other modules
 module.exports = {
     Circle,
     Triangle,
